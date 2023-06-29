58c142adec77d86f315a66ac0a1dd4aab46f86fb
Mac_Developer <araozangel842@gmail.com>
2023-06-29 12:16:55 +0300

    Changes:
    1- Adding more prop types.
    2- Adding and updating dummy data structures.

--- assets/data/chats.ts
+++ assets/data/chats.ts
@@ -4,68 +4,184 @@ export const data: ChatType[] = [
   {
     id: '1',
     user: {
+      id: 'u1',
       image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/woman_ojcard.jpg',
       name: 'Angel',
     },
     lastMessage: {
+      id: 'm1',
       content: 'Hola, como estas?',
-      createdAt: '04:35',
+      createdAt: '2023-06-25T15:30:00.000Z',
     },
   },
   {
     id: '2',
     user: {
+      id: 'u2',
       image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/images_copia_xjhrhp.jpg',
       name: 'Luis',
     },
     lastMessage: {
+      id: 'm2',
       content: 'Hola, como estas?',
-      createdAt: '04:35',
+      createdAt: '2023-06-26T15:30:00.000Z'
     },
   },
   {
     id: '3',
     user: {
+      id: 'u3',
       image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/woman2_ycwcr7.jpg',
       name: 'Carlos',
     },
     lastMessage: {
+      id: 'm3',
       content: 'Hei yooooo?',
-      createdAt: '04:55',
+      createdAt: '2023-06-28T15:30:00.000Z'
     },
   },
   {
     id: '4',
     user: {
+      id: 'u4',
       image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/men_m1qoho.jpg',
       name: 'Jorge',
     },
     lastMessage: {
+      id: 'm4',
       content: 'Hola, como estas?',
-      createdAt: '04:35',
+      createdAt: '2023-05-21T15:30:00.000Z'
     },
   },
   {
     id: '5',
     user: {
+      id: 'u5',
       image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/men1_cxrvum.jpg',
       name: 'Jorge',
     },
     lastMessage: {
+      id: 'm5',
       content: 'Hola, como estas?',
-      createdAt: '04:35',
+      createdAt: '2023-06-28T12:00:00.000Z'
     },
   },
   {
     id: '6',
     user: {
+      id: 'u6',
       image:
-        'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/she_b84e5i.jpg',
+        'https://res.cloudinary.com/dqaerysgb/image/upload/v1628020655/samples/people/kitchen-bar.jpg',
       name: 'Ilda',
     },
     lastMessage: {
+      id: 'm6',
       content: 'Hello there',
-      createdAt: '07:30',
+      createdAt: '2023-06-29T08:30:00.000Z'
     },
   },
+  {
+    id: '7',
+    user: {
+      id: 'u7',
+      image:  'https://res.cloudinary.com/dqaerysgb/image/upload/v1628020656/samples/people/smiling-man.jpg',
+      name: 'Joe',
+    },
+    lastMessage: {
+      id: 'm7',
+      content: 'Hello there',
+      createdAt: '2023-06-21T15:30:00.000Z'
+    },
+  },
+  {
+    id: '8',
+    user: {
+      id: 'u8',
+      image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/she_b84e5i.jpg',
+      name: 'Angela',
+    },
+    lastMessage: {
+      id: 'm8',
+      content: 'Hello there',
+      createdAt: '2023-06-28T15:30:00.000Z'
+    },
+  },
+  {
+    id: '9',
+    user: {
+      id: 'u9',
+      image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1628020658/samples/people/boy-snow-hoodie.jpg',
+      name: 'Carla',
+    },
+    lastMessage: {
+      id: 'm9',
+      content: 'Hello there',
+      createdAt: '2022-10-21T15:30:00.000Z'
+    },
+  },
+  {
+    id: '10',
+    user: {
+      id: 'u10',
+      image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1628020660/samples/people/bicycle.jpg',
+      name: 'Anniina',
+    },
+    lastMessage: {
+      id: 'm10',
+      content: 'Hello there',
+      createdAt: '2023-06-29T05:30:00.000Z'
+    },
+  },
+  {
+    id: '11',
+    user: {
+      id: 'u11',
+      image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1650308582/img-1_j6xpxv.jpg',
+      name: 'Maria',
+    },
+    lastMessage: {
+      id: 'm11',
+      content: 'Hello there',
+      createdAt: '2023-04-21T15:30:00.000Z'
+    },
+  },
+  {
+    id: '12',
+    user: {
+      id: 'u12',
+      image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/she_b84e5i.jpg',
+      name: 'Vera',
+    },
+    lastMessage: {
+      id: 'm12',
+      content: 'Hello there',
+      createdAt: '2023-03-21T15:30:00.000Z'
+    },
+  },
+  {
+    id: '13',
+    user: {
+      id: 'u13',
+      image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1635399087/Cuban-Food-1_mydd1f.jpg',
+      name: 'Ilda',
+    },
+    lastMessage: {
+      id: 'm13',
+      content: 'Hello there',
+      createdAt: '2023-02-21T15:30:00.000Z'
+    },
+  },
+  {
+    id: '14',
+    user: {
+      id: 'u14',
+      image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1628020648/sample.jpg',
+      name: 'Sonia',
+    },
+    lastMessage: {
+      id: 'm14',
+      content: 'Hello there',
+      createdAt: '2023-05-21T15:30:00.000Z'
+    },
+  }
 ];
--- src/types/ChatItemType.ts
+++ src/types/ChatItemType.ts
@@ -1,10 +1,12 @@
 export type ChatType = {
     id: string;
     user: {
+      id: string
       image: string
       name: string
     },
     lastMessage: {
+      id: string
       content: string,
       createdAt: string
     }
