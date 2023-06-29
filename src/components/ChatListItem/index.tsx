51326db5e9404363cbbc131b6556c3e3c50c3ae6
Mac_Developer <araozangel842@gmail.com>
2023-06-29 11:37:55 +0300

    Run prettier

--- src/components/ChatListItem/index.tsx
+++ src/components/ChatListItem/index.tsx
@@ -1,53 +1,57 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import React from 'react'
-import { ChatType } from '../../types/ChatItemType'
+import { View, Text, Image, StyleSheet } from 'react-native';
+import React from 'react';
+import { ChatType } from '../../types/ChatItemType';
 
 type Props = {
-  chat: ChatType
-}
-const ChatListItem = ({chat}: Props) => {
+  chat: ChatType;
+};
+const ChatListItem = ({ chat }: Props) => {
   return (
     <View style={styles.container}>
-      <Image source={{uri: chat.user.image}} style={styles.image} />
+      <Image source={{ uri: chat.user.image }} style={styles.image} />
       <View style={styles.content}>
         <View style={styles.row}>
-          <Text style={styles.name} numberOfLines={1}>{chat.user.name}</Text>
+          <Text style={styles.name} numberOfLines={1}>
+            {chat.user.name}
+          </Text>
           <Text style={styles.subtitle}>{chat.lastMessage.createdAt}</Text>
         </View>
-        <Text numberOfLines={2} style={styles.subtitle}>{chat.lastMessage.text}</Text>
+        <Text numberOfLines={2} style={styles.subtitle}>
+          {chat.lastMessage.content}
+        </Text>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container : {
+  container: {
     flexDirection: 'row',
     marginHorizontal: 14,
     marginVertical: 5,
-    height: 70
+    height: 70,
   },
   image: {
     width: 60,
     height: 60,
     borderRadius: 30,
-    marginRight: 10
+    marginRight: 10,
   },
   content: {
     flex: 1,
     borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: '#7171714E'
+    borderBottomColor: '#7171714E',
   },
-  row:{
-    flexDirection: 'row'
+  row: {
+    flexDirection: 'row',
   },
   name: {
     flex: 1,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   subtitle: {
-    color: '#717171'
-  }
-})
+    color: '#717171',
+  },
+});
 
-export default ChatListItem
+export default ChatListItem;
