508dd35b078179f8b35b448f64a2303296e8518e
Mac_Developer <araozangel842@gmail.com>
2023-06-29 11:50:14 +0300

    Rendering dummy data

--- src/components/Screens/ChatScreen.tsx
+++ src/components/Screens/ChatScreen.tsx
@@ -1,36 +1,14 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import React from 'react';
-import { ChatType } from '../../types/ChatItemType';
+import { data } from '../../../assets/data/chats';
 import ChatListItem from '../ChatListItem';
 
-const chat: ChatType = {
-  id: '1',
-  user: {
-    image:
-      'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/she_b84e5i.jpg',
-    name: 'Ilda',
-  },
-  lastMessage: {
-    content: 'Hello there',
-    createdAt: '07:30',
-  },
-};
 const ChatScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>ChatScreen</Text>
-      <ChatListItem chat={chat} />
-    </View>
+    <SafeAreaView>
+      <FlatList data={data} renderItem={({ item }) => <ChatListItem chat={item} />} />
+    </SafeAreaView>
   );
 };
 
 export default ChatScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
