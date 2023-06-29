22f66108044bdfbab644e6e7efd40f0faf92079f
Mac_Developer <araozangel842@gmail.com>
2023-06-29 13:23:24 +0300

    Add dummy message data

---
+++ assets/data/messages.ts
@@ -0,0 +1,58 @@
+import { MessagesType } from "../../src/types/ChatItemType";
+
+export const messages: MessagesType[] = [
+  {
+    id: '1',
+    content: 'Yoooo, I should be the last message?',
+    createdAt: '2023-06-29T05:30:00.000Z',
+    user: {
+      id: 'u1',
+      name: 'Angel',
+    },
+  },
+  {
+    id: '2',
+    content: 'How are you doing bro? Do you know if there is a new update',
+    createdAt: '2023-06-26T15:30:00.000Z',
+    user: {
+      id: 'u2',
+      name: 'Luis',
+    },
+  },
+  {
+    id: '3',
+    content: 'Hei yooooo? Im here, where are you? Im waiting for you',
+    createdAt: '2023-06-28T15:30:00.000Z',
+    user: {
+      id: 'u3',
+      name: 'Carlos',
+    },
+  },
+  {
+    id: '4',
+    content: 'Cooooooool!!!!!',
+    createdAt: '2023-05-21T15:30:00.000Z',
+    user: {
+      id: 'u4',
+      name: 'Jorge',
+    },
+  },
+  {
+    id: '5',
+    content: 'Dude, are you playing today?',
+    createdAt: '2023-05-27T15:30:00.000Z',
+    user: {
+      id: 'u5',
+      name: 'Jorge',
+    },
+  },
+  {
+    id: '6',
+    content: 'Im fine, just waiting my cooffe?',
+    createdAt: '2023-05-25T15:30:00.000Z',
+    user: {
+      id: 'u6',
+      name: 'Jorge',
+    },
+  }
+]
