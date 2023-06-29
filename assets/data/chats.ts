54d500603e3063fc640d6dadc9b17a15bd395f66
Mac_Developer <araozangel842@gmail.com>
2023-06-29 11:37:18 +0300

    Add dummy data

---
+++ assets/data/chats.ts
@@ -0,0 +1,71 @@
+import { ChatType } from "../../src/types/ChatItemType";
+
+export const data: ChatType[] = [
+  {
+    id: '1',
+    user: {
+      image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/woman_ojcard.jpg',
+      name: 'Angel',
+    },
+    lastMessage: {
+      content: 'Hola, como estas?',
+      createdAt: '04:35',
+    },
+  },
+  {
+    id: '2',
+    user: {
+      image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/images_copia_xjhrhp.jpg',
+      name: 'Luis',
+    },
+    lastMessage: {
+      content: 'Hola, como estas?',
+      createdAt: '04:35',
+    },
+  },
+  {
+    id: '3',
+    user: {
+      image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/woman2_ycwcr7.jpg',
+      name: 'Carlos',
+    },
+    lastMessage: {
+      content: 'Hei yooooo?',
+      createdAt: '04:55',
+    },
+  },
+  {
+    id: '4',
+    user: {
+      image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/men_m1qoho.jpg',
+      name: 'Jorge',
+    },
+    lastMessage: {
+      content: 'Hola, como estas?',
+      createdAt: '04:35',
+    },
+  },
+  {
+    id: '5',
+    user: {
+      image: 'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/men1_cxrvum.jpg',
+      name: 'Jorge',
+    },
+    lastMessage: {
+      content: 'Hola, como estas?',
+      createdAt: '04:35',
+    },
+  },
+  {
+    id: '6',
+    user: {
+      image:
+        'https://res.cloudinary.com/dqaerysgb/image/upload/v1687820061/samples/people/she_b84e5i.jpg',
+      name: 'Ilda',
+    },
+    lastMessage: {
+      content: 'Hello there',
+      createdAt: '07:30',
+    },
+  },
+];
