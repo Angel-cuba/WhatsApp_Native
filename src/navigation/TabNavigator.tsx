5944629102ea5fecdfab93df2a6ed8f22f0e1179
Mac_Developer <araozangel842@gmail.com>
2023-07-01 12:12:33 +0300

    Add icons to tabs

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,20 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons, Entypo } from '@expo/vector-icons';
 import NotScreen from '../components/Screens/NotScreen';
 import ChatsScreen from '../components/Screens/ChatsScreen';
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Status" component={NotScreen} />
-      <Tab.Screen name="Calls" component={NotScreen} />
-      <Tab.Screen name="Camera" component={NotScreen} />
-      <Tab.Screen name="Chats" component={ChatsScreen} />
-      <Tab.Screen name="Settings" component={NotScreen} />
+    <Tab.Navigator initialRouteName='Chats' screenOptions={{
+      tabBarStyle: {
+        backgroundColor: '#fafafa',
+      }
+    }}>
+      <Tab.Screen
+        name="Status"
+        component={NotScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="logo-whatsapp" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Calls"
+        component={NotScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <Ionicons name="call-outline" size={size} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="Camera"
+        component={NotScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <Ionicons name="camera-outline" size={size} color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="Chats"
+        component={ChatsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="chatbubbles" size={size} color={color} />
+          ),
+          headerRight: () => (
+            <Entypo name="new-message" size={24} color="black" style={{
+              marginRight: 20
+            }}/>
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={NotScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <Ionicons name="settings-outline" size={size} color={color} />,
+        }}
+      />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
-export default TabNavigator
+export default TabNavigator;
