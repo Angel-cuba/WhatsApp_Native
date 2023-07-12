32570fb820592808fe45cf7dc8c4dc3e7ea66218
Mac_Developer <araozangel842@gmail.com>
2023-07-12 13:29:24 +0300

    Fetch user data fron db if exists, otherwise create new one.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Navigator from './src/navigation';
-import { Amplify } from 'aws-amplify';
+import { API, Amplify, Auth, graphqlOperation } from 'aws-amplify';
 import awsconfig from './src/aws-exports';
 import { withAuthenticator } from 'aws-amplify-react-native';
+import { useEffect } from 'react';
+import { getUser } from './src/graphql/queries';
+import { createUser } from './src/graphql/mutations';
 
 Amplify.configure({
   ...awsconfig,
@@ -12,7 +15,42 @@ Amplify.configure({
   },
 });
 
+type userData = {
+  id: string;
+  name: string;
+  image: string;
+  status: string;
+}
+type dataProps = {
+  data: {
+    getUser: userData
+  }
+}
+
 function App() {
+  useEffect(() => {
+    const user = async () => {
+        const user = await Auth.currentAuthenticatedUser({
+          bypassCache: true,
+        });
+    const userData  = await API.graphql(graphqlOperation(getUser, {
+      id: user.attributes.sub
+    })) as dataProps
+    if(userData.data.getUser) {
+      console.log("User is already registered in database");
+      return;
+    }
+    const newUser = {
+      id: user.attributes.sub,
+      name: user.email,
+      image: 'https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png',
+      status: 'Hey, I am using WhatsApp',
+    }
+    await API.graphql(graphqlOperation(createUser, {input: newUser}))
+    }
+    user();
+  }, [])
+  
   return (
     <View style={styles.container}>
       <Navigator />
