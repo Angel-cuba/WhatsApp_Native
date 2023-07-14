1aae5503328afd2924acb806f1ac46bd49d43e9e
Mac_Developer <araozangel842@gmail.com>
2023-07-14 21:31:53 +0300

    Method to get messages from a chat room

--- src/components/Screens/Chat.tsx
+++ src/components/Screens/Chat.tsx
@@ -4,13 +4,15 @@ import {
   FlatList,
   KeyboardAvoidingView,
   Platform,
+  ActivityIndicator,
 } from 'react-native';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import { messages } from '../../../assets/data/messages';
 import Message from '../Message';
 import InputBox from '../InputBox';
-import { Auth } from 'aws-amplify';
+import { API, Auth, graphqlOperation } from 'aws-amplify';
+import { getChatRoom } from '../../graphql/queries';
 
 type Props = {
   id: string;
@@ -20,31 +22,62 @@ type Props = {
       name: string;
       image: string;
     };
-  }[]
+  }[];
+};
+
+type chatRoomType = {
+  data: {
+    getChatRoom: {
+      LastMessage: string;
+      Messages: {
+        items: {
+          createdAt: string;
+          id: string;
+          image: string;
+          message: string;
+          chatroomID: string;
+          userID: string;
+        };
+      };
+    };
+  };
 };
 
 const Chat = () => {
   const [user, setUser] = useState(null);
+  const [chatRoom, setChatRoom] = useState(null);
   const route = useRoute();
   const { id, users } = route.params as Props;
   const navigation = useNavigation();
 
   useEffect(() => {
-        const authUser = async () => {
+    const fetchChatRoom = async () => {
+      const response = (await API.graphql(
+        graphqlOperation(getChatRoom, {
+          id: id,
+        })
+      )) as chatRoomType;
+      setChatRoom(response.data?.getChatRoom);
+    };
+    fetchChatRoom();
+  }, []);
+
+  useEffect(() => {
+    const authUser = async () => {
       const user = await Auth.currentAuthenticatedUser();
 
-      const userItem = users.find(
-        (item) => item.user.id !== user.attributes.sub
-      );
+      const userItem = users.find((item) => item.user.id !== user.attributes.sub);
       setUser(userItem?.user);
     };
     authUser();
   }, [id, users]);
-    navigation.setOptions({
-      title: id
-        ? `${user?.name}` // if there is an id, then it is a chat room
-        : 'Chat', // otherwise, it is a user chat
-    });
+  navigation.setOptions({
+    title: `${user?.name}`,
+  });
+
+  if (!chatRoom) {
+    return <ActivityIndicator />;
+  }
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -59,12 +92,12 @@ const Chat = () => {
         blurRadius={2}
       >
         <FlatList
-          data={messages}
+          data={chatRoom?.Messages.items}
           renderItem={({ item }) => <Message message={item} />}
           style={styles.list}
           inverted
         />
-        <InputBox />
+        <InputBox chatRoomId={id} />
       </ImageBackground>
     </KeyboardAvoidingView>
   );
